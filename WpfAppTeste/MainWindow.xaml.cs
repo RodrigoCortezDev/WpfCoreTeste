a9941bc477bac01e96adb84126edb8ec78a74a4c
Rodrigo Cortez <Rodrigo Cortez>
2021-02-12 18:27:14 -0300

    Teste

--- WpfAppTeste/MainWindow.xaml.cs
+++ WpfAppTeste/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfAppTeste
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("aaaaa");
+        }
     }
 }
